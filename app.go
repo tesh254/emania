fbe129cc9b8601f846e73ca13676a37844ea0831
tesh254 <ewachira254@gmail.com>
2020-02-03 08:38:04 +0300

    ft(auth): api to handle authentication
    - create endpoint to register user
    - add email verification send out
    - add login endpoint to give user access to their account
    - add verify email account endpoint
    - add token blacklisting

--- app.go
+++ app.go
@@ -1,18 +1,38 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+	"log"
 
-	"github.com/gorilla/mux"
+	"github.com/tesh254/packit/controllers"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
-	r := mux.NewRouter()
+	router := gin.Default()
 
-	if err := http.ListenAndServe(":7000", r); err != nil {
-		log.Fatal(err)
+	v1 := router.Group("/api/v1") 
+	{
+		// User related endpoints
+		users := new(controllers.UserController)
+		v1.GET("/users", users.Find)
+		v1.POST("/signup", users.Signup)
+		v1.POST("/login", users.Login)
+		v1.PUT("/email-verify", users.Verify)
+		v1.POST("/password-reset", users.PasswordRequest)
+		v1.PUT("/password-reset-submit", users.PasswordResetSubmit)
 	}
+
+	router.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusNotFound, "Not Found")
+	})
+
+	router.Run(":5000")
 }
